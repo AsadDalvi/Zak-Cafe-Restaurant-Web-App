9520b5e04bcdc6f1880f12357fb8f49b9f2f5ea4
AsadDalvi <191914306+AsadDalvi@users.noreply.github.com>
2026-06-09 00:22:56 +0400

    settings.py

--- zakcafe/settings.py
+++ zakcafe/settings.py
@@ -1,3 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 """
 Django settings for zakcafe project.
 
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9hqyh8*#co#hvdh$2b3bvx1_i1p+*qkc2b%o5@ut@yq%-tvf_p'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
